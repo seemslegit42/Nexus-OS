ea52faa1b837057cf838325b238a22e8cff7e34c
Bryan Marlow <brylow@aevon.cloud>
2025-06-13 05:05:24 +0000

    continue

--- src/components/core/ActiveAgentsPopoverContent.tsx
+++ src/components/core/ActiveAgentsPopoverContent.tsx
@@ -1,5 +1,3 @@
-
 // This file is no longer needed as its functionality is superseded by AgentWorkloadPreview.tsx
 // The TopBar now uses AgentWorkloadPreview for the CPU icon popover.
 // You can safely delete this file.
-// src/components/core/ActiveAgentsPopoverContent.tsx
